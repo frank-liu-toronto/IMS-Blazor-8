6361c32f4a4388049ca551371a3e8b0ecbc3bfb7
Frank Liu <liu1000@gmail.com>
2024-05-22 16:16:43 -0400

    Form Validations with Data Annotations

--- IMS.CoreBusiness/Inventory.cs
+++ IMS.CoreBusiness/Inventory.cs
@@ -1,10 +1,19 @@
-﻿namespace IMS.CoreBusiness
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IMS.CoreBusiness
 {
     public class Inventory
     {
         public int InventoryId { get; set; }
+
+        [Required]
+        [StringLength(150)]
         public string InventoryName { get; set; } = string.Empty;
+
+        [Range(0, int.MaxValue, ErrorMessage = "Quantity must be greator or equal to 0.")]
         public int Quantity { get; set; }
+
+        [Range(0, int.MaxValue, ErrorMessage = "Price must be greator or equal to 0.")]
         public double Price { get; set; }
     }
 }
