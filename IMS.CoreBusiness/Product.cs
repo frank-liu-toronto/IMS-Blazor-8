cd4cf02262f41a91fe613d38d5e860a28c4b09fa
Frank Liu <liu1000@gmail.com>
2024-05-28 15:06:30 -0400

    Add Inventory to Product

--- IMS.CoreBusiness/Product.cs
+++ IMS.CoreBusiness/Product.cs
@@ -15,5 +15,30 @@ namespace IMS.CoreBusiness
 
         [Range(0, int.MaxValue, ErrorMessage = "Price must be greator or equal to 0.")]
         public double Price { get; set; }
+
+        public List<ProductInventory> ProductInventories { get; set; } = new List<ProductInventory>();
+
+        public void AddInventory(Inventory inventory)
+        {
+            if (!this.ProductInventories.Any(
+                x => x.Inventory is not null && 
+                x.Inventory.InventoryName.Equals(inventory.InventoryName)))
+            {
+                this.ProductInventories.Add(new ProductInventory
+                {
+                    InventoryId = inventory.InventoryId,
+                    Inventory = inventory,
+                    InventoryQuantity = 1,
+                    ProductId = this.ProductId,
+                    Product = this
+                });
+            }            
+        }
+
+        public void RemoveInventory(ProductInventory productInventory) 
+        {
+            this.ProductInventories?.Remove(productInventory);
+        }
+
     }
 }
---
+++ IMS.CoreBusiness/ProductInventory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace IMS.CoreBusiness
+{
+    public class ProductInventory
+    {
+        public int ProductId { get; set; }
+
+        [JsonIgnore]
+        public Product? Product { get; set; }
+
+        public int InventoryId { get; set; }
+
+        [JsonIgnore]
+        public Inventory? Inventory { get; set; }
+
+        public int InventoryQuantity { get; set; }
+    }
+}
