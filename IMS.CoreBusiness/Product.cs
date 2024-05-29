6e30c3a431508cc80ff3ebfa659c8886fffabd3e
Frank Liu <liu1000@gmail.com>
2024-05-29 16:43:02 -0400

    Use ValidationAttribute for Custom Validations

--- IMS.CoreBusiness/Product.cs
+++ IMS.CoreBusiness/Product.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using IMS.CoreBusiness.Validations;
+using System.ComponentModel.DataAnnotations;
 
 namespace IMS.CoreBusiness
 {
@@ -16,6 +17,7 @@ namespace IMS.CoreBusiness
         [Range(0, int.MaxValue, ErrorMessage = "Price must be greator or equal to 0.")]
         public double Price { get; set; }
 
+        [Product_EnsurePriceGreaterThanInventoriesCost]
         public List<ProductInventory> ProductInventories { get; set; } = new List<ProductInventory>();
 
         public void AddInventory(Inventory inventory)
---
+++ IMS.CoreBusiness/Validations/Product_EnsurePriceGreaterThanInventoriesCost.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMS.CoreBusiness.Validations
+{
+    public class Product_EnsurePriceGreaterThanInventoriesCost : ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            var product = validationContext.ObjectInstance as Product;
+            if (product != null)
+            {
+                if (!ValidatePricing(product))
+                    return new ValidationResult($"The product's price is less than the inventories cost: {TotalInventoriesCost(product).ToString("c")}!",
+                        new List<string>() { validationContext.MemberName });
+            }
+
+            return ValidationResult.Success;
+        }
+
+        private double TotalInventoriesCost(Product product)
+        {
+            if (product is null || product.ProductInventories is null) return 0;
+
+            return product.ProductInventories.Sum(x => x.Inventory?.Price * x.InventoryQuantity ?? 0);
+        }
+
+        private bool ValidatePricing(Product product)
+        {
+            if (product is null || 
+                product.ProductInventories is null ||  
+                product.ProductInventories.Count <= 0) return true;
+
+            if (TotalInventoriesCost(product) > product.Price) return false;
+
+            return true;
+        }
+
+
+    }
+}
