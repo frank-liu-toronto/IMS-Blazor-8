c8e809268e0aed1cbe475a56166b5aa6e1a9cf1c
Frank Liu <liu1000@gmail.com>
2024-05-31 15:59:04 -0400

    Custom Validation for Quantity to Produce

---
+++ IMS.WebApp/ViewModels/ProduceViewModel.cs
@@ -0,0 +1,21 @@
+﻿using IMS.CoreBusiness;
+using IMS.WebApp.ViewModelsValidations;
+using System.ComponentModel.DataAnnotations;
+
+namespace IMS.WebApp.ViewModels
+{
+    public class ProduceViewModel
+    {
+        [Required]
+        public string ProductionNumber { get; set; } = string.Empty;
+
+        [Range(minimum: 1, maximum:int.MaxValue, ErrorMessage = "You have to select a product.")]
+        public int ProductId { get; set; }
+
+        [Range(minimum: 1, maximum: int.MaxValue, ErrorMessage = "Quantity has to be greater or equal to 1.")]
+        [Produce_EnsureEnoughInventoryQuantity]
+        public int QuantityToProduce { get; set; }
+
+        public Product? Product { get; set; }
+    }
+}
---
+++ IMS.WebApp/ViewModelsValidations/Produce_EnsureEnoughInventoryQuantity.cs
@@ -0,0 +1,30 @@
+﻿using IMS.WebApp.ViewModels;
+using System.ComponentModel.DataAnnotations;
+
+namespace IMS.WebApp.ViewModelsValidations
+{
+    public class Produce_EnsureEnoughInventoryQuantity : ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            var produceViewModel = validationContext.ObjectInstance as ProduceViewModel;
+            if ( produceViewModel is not null)
+            {
+                if (produceViewModel.Product != null && produceViewModel.Product.ProductInventories != null)
+                {
+                    foreach (var pi in produceViewModel.Product.ProductInventories)
+                    {
+                        if (pi.Inventory != null &&
+                            pi.InventoryQuantity * produceViewModel.QuantityToProduce > pi.Inventory.Quantity)
+                        {
+                            return new ValidationResult($"The inventory ({pi.Inventory.InventoryName}) is not enough to produce {produceViewModel.QuantityToProduce} products",
+                                new[] { validationContext.MemberName });
+                        }
+                    }
+                }
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
