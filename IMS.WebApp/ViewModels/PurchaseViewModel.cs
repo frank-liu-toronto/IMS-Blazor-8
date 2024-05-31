81b26669a503f160f19677320dd657d9f267315d
Frank Liu <liu1000@gmail.com>
2024-05-31 13:13:54 -0400

    Purchase Inventory Page Component

---
+++ IMS.WebApp/ViewModels/PurchaseViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IMS.WebApp.ViewModels
+{
+    public class PurchaseViewModel
+    {
+        [Required]
+        public string PONumber { get; set; } = string.Empty;
+
+        [Range(minimum: 1, maximum:int.MaxValue, ErrorMessage = "You have to select an inventory.")]
+        public int InventoryId { get; set; }
+
+        [Range(minimum: 1, maximum: int.MaxValue, ErrorMessage = "Quantity has to be greater or equal to 1.")]
+        public int QuantityToPurchase { get; set; }
+
+        public double InventoryPrice { get; set; }
+    }
+}
