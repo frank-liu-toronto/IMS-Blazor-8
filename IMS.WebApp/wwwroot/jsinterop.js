1665a386452ab81380f2168c9e182ad426250ee0
Frank Liu <liu1000@gmail.com>
2024-06-03 11:02:47 -0400

    Javascript InterOp

---
+++ IMS.WebApp/wwwroot/jsinterop.js
@@ -0,0 +1,12 @@
+﻿function preventFormSubmission(formId) {
+
+    document.getElementById(`${formId}`).addEventListener('keydown', function (event) {
+
+        if (event.key == "Enter") {
+            event.preventDefault();
+            return false;
+        }
+
+    });
+
+}
