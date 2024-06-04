d8ed090afa10441c6dfc14d0a16fdb77bfa88d14
Frank Liu <liu1000@gmail.com>
2024-06-04 10:32:42 -0400

    Inventory Transaction Report Page Component

--- IMS.UseCases/Reports/SearchInventoryTransactionsUseCase.cs
+++ IMS.UseCases/Reports/SearchInventoryTransactionsUseCase.cs
@@ -25,6 +25,8 @@ namespace IMS.UseCases.Reports
             InventoryTransactionType? transactionType
             )
         {
+            if (dateTo.HasValue) dateTo = dateTo.Value.AddDays(1);
+
             return await this.inventoryTransactionRepository.GetInventoryTransactionAsync(
                     inventoryName,
                     dateFrom,
