6f4a9bbb815da64335d78716c5b64ce1da866534
Frank Liu <liu1000@gmail.com>
2024-06-05 16:38:44 -0400

    Inventory Repository with EF Core

---
+++ IMS.Plugins/IMS.Plugins.EFCoreSqlServer/InventoryEFCoreRepository.cs
@@ -0,0 +1,67 @@
+﻿using IMS.CoreBusiness;
+using IMS.UseCases.PluginInterfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMS.Plugins.EFCoreSqlServer
+{
+    public class InventoryEFCoreRepository: IInventoryRepository
+    {
+        private readonly IDbContextFactory<IMSContext> contextFactory;
+
+        public InventoryEFCoreRepository(IDbContextFactory<IMSContext> contextFactory)
+        {
+            this.contextFactory = contextFactory;
+        }
+
+        public async Task AddInventoryAsync(Inventory inventory)
+        {
+            using var db = this.contextFactory.CreateDbContext();
+            db.Inventories?.Add(inventory);
+            await db.SaveChangesAsync();
+        }
+
+        public async Task DeleteInventoryByIdAsync(int inventoryId)
+        {
+            using var db = this.contextFactory.CreateDbContext();
+            var inventory = db.Inventories?.Find(inventoryId);
+            if (inventory is null) return;
+
+            db.Inventories?.Remove(inventory);
+            await db.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<Inventory>> GetInventoriesByNameAsync(string name)
+        {
+            using var db = this.contextFactory.CreateDbContext();
+            return await db.Inventories.Where(x => x.InventoryName.ToLower().IndexOf(name.ToLower()) >= 0).ToListAsync();
+        }
+
+        public async Task<Inventory> GetInventoryByIdAsync(int inventoryId)
+        {
+            using var db = this.contextFactory.CreateDbContext();
+            var inventory = await db.Inventories.FindAsync(inventoryId);
+            if (inventory is not null) return inventory;
+
+            return new Inventory();
+        }
+
+        public async Task UpdateInventoryAsync(Inventory inventory)
+        {
+            using var db = this.contextFactory.CreateDbContext();
+            var inv = await db.Inventories.FindAsync(inventory.InventoryId);
+            if (inv is not null)
+            {
+                inv.InventoryName = inventory.InventoryName;
+                inv.Price = inventory.Price;
+                inv.Quantity = inventory.Quantity;
+
+                await db.SaveChangesAsync();
+            }
+        }
+    }
+}
