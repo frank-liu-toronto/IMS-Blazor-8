04d688e60e138726f7cf20022f838903fb42a937
Frank Liu <liu1000@gmail.com>
2024-06-06 17:49:39 -0400

    Solve Blazor SSR EditForm issues

---
+++ IMS.WebApp/ViewModels/InventoryViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IMS.CoreBusiness
+{
+    public class InventoryViewModel
+    {
+        public int InventoryId { get; set; }
+
+        [Required]
+        [StringLength(150)]
+        public string InventoryName { get; set; } = string.Empty;
+
+        [Range(0, int.MaxValue, ErrorMessage = "Quantity must be greator or equal to 0.")]
+        public int Quantity { get; set; }
+
+        [Range(0, int.MaxValue, ErrorMessage = "Price must be greator or equal to 0.")]
+        public double Price { get; set; }
+        
+    }
+}
