bb9b3f4144ffb2be3cbf2d6099a136fdece00a4a
Frank Liu <liu1000@gmail.com>
2024-06-06 14:00:53 -0400

    Inventory Transaction Repository with EF Core

---
+++ IMS.Plugins/IMS.Plugins.EFCoreSqlServer/InventoryTransactionEFCoreRepository.cs
@@ -0,0 +1,77 @@
+﻿using IMS.CoreBusiness;
+using IMS.UseCases.PluginInterfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMS.Plugins.EFCoreSqlServer
+{
+    public class InventoryTransactionEFCoreRepository : IInventoryTransactionRepository
+    {
+        private readonly IDbContextFactory<IMSContext> contextFactory;
+
+        public InventoryTransactionEFCoreRepository(IDbContextFactory<IMSContext> contextFactory)
+        {
+            this.contextFactory = contextFactory;
+        }
+
+        public async Task<IEnumerable<InventoryTransaction>> GetInventoryTransactionAsync(string inventoryName, DateTime? dateFrom, DateTime? dateTo, InventoryTransactionType? transactionType)
+        {
+            using var db = contextFactory.CreateDbContext();
+
+            var query = from it in db.InventoryTransactions
+                        join inv in db.Inventories on it.InventoryId equals inv.InventoryId
+                        where
+                            (string.IsNullOrWhiteSpace(inventoryName) || inv.InventoryName.ToLower().IndexOf(inventoryName.ToLower()) >= 0)
+                            &&
+                            (!dateFrom.HasValue || it.TransactionDate >= dateFrom.Value.Date) &&
+                            (!dateTo.HasValue || it.TransactionDate <= dateTo.Value.Date) &&
+                            (!transactionType.HasValue || it.ActivityType == transactionType)
+                        select it;
+
+            return await query.Include(x => x.Inventory).ToListAsync();
+        }
+
+        public async Task ProduceAsync(string productionNumber, Inventory inventory, int quantityToConsume, string doneBy, double price)
+        {
+            using var db = contextFactory.CreateDbContext();
+
+            db.InventoryTransactions?.Add(new InventoryTransaction
+            {
+                ProdutionNumber = productionNumber,
+                InventoryId = inventory.InventoryId,
+                QuantityBefore = inventory.Quantity,
+                ActivityType = InventoryTransactionType.ProduceProduct,
+                QuantityAfter = inventory.Quantity - quantityToConsume,
+                TransactionDate = DateTime.Now,
+                DoneBy = doneBy,
+                UnitPrice = price
+            });
+
+            await db.SaveChangesAsync();
+        }
+
+        public async Task PurchaseAsync(string poNumber, Inventory inventory, int quantity, string doneBy, double price)
+        {
+            using var db = contextFactory.CreateDbContext();
+
+            db.InventoryTransactions?.Add(new InventoryTransaction
+            {
+                PONumber = poNumber,
+                InventoryId = inventory.InventoryId,
+                QuantityBefore = inventory.Quantity,
+                ActivityType = InventoryTransactionType.PurchaseInventory,
+                QuantityAfter = inventory.Quantity + quantity,
+                TransactionDate = DateTime.Now,
+                DoneBy = doneBy,
+                UnitPrice = price
+            });
+
+            await db.SaveChangesAsync();
+        }
+    }
+}
